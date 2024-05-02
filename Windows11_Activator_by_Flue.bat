ae3771af727c5ff1a324eae623cbcc84919a65aa
Flue <97408371+LueFlue@users.noreply.github.com>
2024-05-02 18:30:35 +0200

    Add files via upload

---
+++ Windows11_Activator_by_Flue.bat
@@ -0,0 +1,322 @@
+@echo off
+chcp 65001 >nul
+
+Copyright Flue. All right reserved.
+
+:menu
+cls
+title Windows 11 Activator by Flue
+color 09
+echo.
+echo ======================================================================================================================
+echo                   ###     ######  ######## #### ##     ##    ###    ########  #######  ########  
+echo                  ## ##   ##    ##    ##     ##  ##     ##   ## ##      ##    ##     ## ##     ## 
+echo                 ##   ##  ##          ##     ##  ##     ##  ##   ##     ##    ##     ## ##     ## 
+echo                ##     ## ##          ##     ##  ##     ## ##     ##    ##    ##     ## ########  
+echo                ######### ##          ##     ##   ##   ##  #########    ##    ##     ## ##   ##   
+echo                ##     ## ##    ##    ##     ##    ## ##   ##     ##    ##    ##     ## ##    ##  
+echo                ##     ##  ######     ##    ####    ###    ##     ##    ##     #######  ##     ## 
+echo ======================================================================================================================
+echo.
+echo                                          !!!!RUN AS ADMINISTRATOR!!!!
+echo.
+echo                                       Copyright Flue. All right reserved.
+echo.    
+echo                                         What do you want of Windows 11?
+echo.
+echo ======================================================================================================================
+echo         [1] Windows 11 Home         [5] Windows 11 Pro               [11] Windows 11 Enterprise
+echo         [2] Windows 11 Home N       [6] Windows 11 Pro N             [12] Windows 11 Enterprise N
+echo         [3] Windows 11 Home HSL     [7] Windows 11 Pro Work          [13] Windows 11 Enterprise G 
+echo         [4] Windows 11 Home HCS     [8] Windows 11 Pro  Work N       [14] Windows 11 Enterprise G N
+echo         [5] Windows 11 Education N  [9] Windows 11 Pro Education     [15] Windows 11 Enterprise LTSC 2019   
+echo         [6] Windows 11 Education    [10] Windows 11 Pro Education N  [16] Windows 11 Enterprise N LTSC 2019
+echo                                                                      [17] Windows 11 Enterprise LTSB 2016
+echo                             Made by Flue                             [18] Windows 11 Enterprise N LTSB 2016
+echo ======================================================================================================================
+set /p choice=      Choice:
+if "%choice%" == "" goto :error
+if "%choice%" == "0" goto :error
+if "%choice%" == "1" goto :home1
+if "%choice%" == "2" goto :home2
+if "%choice%" == "3" goto :home4
+if "%choice%" == "4" goto :pro
+if "%choice%" == "5" goto :pro2
+if "%choice%" == "6" goto :pro3
+if "%choice%" == "7" goto :pro4
+if "%choice%" == "8" goto :pro5
+if "%choice%" == "9" goto :edu1
+if "%choice%" == "10" goto :edu2
+if "%choice%" == "11" goto :enter1
+if "%choice%" == "12" goto :enter2
+if "%choice%" == "13" goto :enter3
+if "%choice%" == "14" goto :enter4
+if "%choice%" == "15" goto :enter5
+if "%choice%" == "16" goto :enter6
+if "%choice%" == "17" goto :enter7
+if "%choice%" == "18" goto :enter8
+if "%choice%" == "19" goto :error
+
+:home1
+title Home
+color 07
+echo.
+echo Please Wait.
+slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+echo Windows 11 Home activated!
+goto :ended
+
+:home2
+title Home N
+color 07
+echo.
+echo Please Wait.
+slmgr /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM
+echo Windows 11 Home N activated!
+goto :ended
+
+:home3
+title Home Single Language
+color 07
+echo.
+echo Please Wait.
+slmgr /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH
+echo Windows 11 Home Single Language activated!
+goto :ended
+
+:home4
+title Home Country Specific
+color 07
+echo.
+slmgr /ipk PVMJN-6DFY6-9CCP6-7BKTT-D3WVR
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk PVMJN-6DFY6-9CCP6-7BKTT-D3WVR
+echo Windows 11 Home Country Specific activated!
+goto :ended
+
+:pro1
+title Pro
+color 03
+echo.
+echo Please Wait.
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+echo Windows 11 Professional activated!
+goto :ended
+
+:pro2
+title Pro N
+color 03
+echo.
+echo Please Wait.
+slmgr /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+echo Windows 11 Professional N activated!
+goto :ended
+
+:pro3
+title Pro Workstations
+color 03
+echo.
+echo Please Wait.
+slmgr /ipk NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
+echo Windows 11 Professional Workstations activated!
+goto :ended
+
+:pro4
+title Pro Workstations N
+color 03
+echo.
+echo Please Wait.
+slmgr /ipk 9FNHH-K3HBT-3W4TD-6383H-6XYWF
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk 9FNHH-K3HBT-3W4TD-6383H-6XYWF
+echo Windows 11 Professional Workstations N!
+goto :ended
+
+:pro5
+title Pro Education
+color 03
+echo.
+echo Please Wait.
+slmgr /ipk 6TP4R-GNPTD-KYYHQ-7B7DP-J447Y
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk 6TP4R-GNPTD-KYYHQ-7B7DP-J447Y
+echo Windows 11 Professional Education activated!
+goto :ended
+
+:edu1
+title Education
+color 04
+echo.
+echo Please Wait.
+slmgr /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+echo Windows 11 Education activated!
+goto :ended
+
+:edu2
+title Education N
+color 04
+echo.
+echo Please Wait.
+slmgr /ipk 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ
+echo Windows 1 Education N activated!
+goto :ended
+
+:enter1
+title Enterprise
+color 04
+echo.
+echo Please Wait.
+slmgr /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
+echo Windows 11 Enterprise activated!
+goto :ended
+
+
+:enter2
+title Enterprise N
+color 04
+echo.
+echo Please Wait.
+slmgr /ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
+echo Windows 11 Enterprise N activated!
+goto :ended
+
+:enter3
+title Enterprise G
+color 04
+echo.
+echo Please Wait.
+slmgr /ipk YYVX9-NTFWV-6MDM3-9PT4T-4M68B
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk YYVX9-NTFWV-6MDM3-9PT4T-4M68B
+echo Windows 11 Enterprise G activated!"
+goto :ended
+
+:enter4
+title Enterprise G N
+color 04
+echo.
+echo Please Wait.
+slmgr /ipk 44RPN-FTY23-9VTTB-MP9BX-T84FV
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk 44RPN-FTY23-9VTTB-MP9BX-T84FV
+echo Windows 11 Enterprise G N activated!
+goto :ended
+
+:enter5
+title Enterprise LTSC 2019
+color 04
+echo.
+echo Please Wait.
+slmgr /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+echo Windows 11 Enterprise LTSC 2019 activated!
+goto :ended
+
+:enter6
+title Enterprise N LTSC 2019
+color 04
+echo.
+echo Please Wait.
+slmgr /ipk 92NFX-8DJQP-P6BBQ-THF9C-7CG2H
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk 92NFX-8DJQP-P6BBQ-THF9C-7CG2H
+echo Windows 11 Enterprise N LTSC 2019 activated!
+goto :ended
+
+:enter7
+title Enterprise LTSB 2016
+color 04
+echo.
+echo Please Wait.
+slmgr /ipk DCPHK-NFMTC-H88MJ-PFHPY-QJ4BJ
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk DCPHK-NFMTC-H88MJ-PFHPY-QJ4BJ
+echo Windows 11 Enterprise LTSB 2016 activated!
+goto :ended
+
+:enter8
+title Enterprise N LTSB 2016
+color 04
+echo.
+echo Please Wait.
+slmgr /ipk QFFDN-GRT3P-VKWWX-X7T3R-8B639
+slmgr /skms kms8.MSGuides.com
+slmgr /ato
+slmgr /ipk QFFDN-GRT3P-VKWWX-X7T3R-8B639
+echo Windows 11 Enterprise N LTSB 2016 activated!
+goto :ended
+
+:error
+title ERROR
+cls
+color 04
+echo =================================================================================
+echo    ###     ######  ######## #### ##     ##    ###    ########  #######  ########  
+echo   ## ##   ##    ##    ##     ##  ##     ##   ## ##      ##    ##     ## ##     ## 
+echo  ##   ##  ##          ##     ##  ##     ##  ##   ##     ##    ##     ## ##     ## 
+echo ##     ## ##          ##     ##  ##     ## ##     ##    ##    ##     ## ########  
+echo ######### ##          ##     ##   ##   ##  #########    ##    ##     ## ##   ##   
+echo ##     ## ##    ##    ##     ##    ## ##   ##     ##    ##    ##     ## ##    ##  
+echo ##     ##  ######     ##    ####    ###    ##     ##    ##     #######  ##     ## 
+echo =================================================================================
+echo.
+echo.
+echo      You cant activate windows right now or it doesnet work on your device,
+echo                     or your device allready have windows activated.
+pause
+exit
+timeout /t 60 /nobreak >nul
+exit
+
+:ended
+cls
+color 08
+echo.
+echo.
+echo Hello the end is here. Thanks for using Windows 11 Activator by Flue.
+
+
+echo Github: https://github.com/LueFlue
+echo Youtube: https://youtube.com@denrigtigeflue
+echo Twitch: https://twitch.tv/denrigtigeflue
+
+timeout /t 25 /nobreak >nul
